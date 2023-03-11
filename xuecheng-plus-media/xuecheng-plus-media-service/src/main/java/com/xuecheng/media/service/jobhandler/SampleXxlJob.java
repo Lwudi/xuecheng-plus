7295e5b9a610ff4c1fcf892903cfe99b3e345212
root <1067416620@qq.com>
2023-03-11 02:50:59 +0000

    更新 'xuecheng-plus-media/xuecheng-plus-media-service/src/main/java/com/xuecheng/media/service/jobhandler/SampleXxlJob.java'

--- xuecheng-plus-media/xuecheng-plus-media-service/src/main/java/com/xuecheng/media/service/jobhandler/SampleXxlJob.java
+++ xuecheng-plus-media/xuecheng-plus-media-service/src/main/java/com/xuecheng/media/service/jobhandler/SampleXxlJob.java
@@ -39,6 +39,8 @@ public class SampleXxlJob {
     @XxlJob("demoJobHandler")
     public void demoJobHandler() throws Exception {
         System.out.println("处理视频.......");
+      System.out.println("处理视频123.......");
+      
 
 
     }
